51188c51aa54d580a22d7f7bff84874bcdb7c125
David <duprazdavid@gmail.com>
2020-03-13 14:10:50 +0100

    creation of projects

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+	    //launch client and server here
+    }
+}
---
+++ src/com/company/Server.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Server
+{
+    public Server()
+    {
+        
+    }
+}
