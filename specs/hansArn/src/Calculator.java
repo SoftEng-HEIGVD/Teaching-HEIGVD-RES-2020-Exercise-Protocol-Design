09a6fad34a61e4c39df7325943d1319ddf52ad1c
jerome arn <jerome.arn@heig-vd.ch>
2020-03-18 20:54:30 +0100

    add calcul

--- specs/hansArn/src/Calculator.java
+++ specs/hansArn/src/Calculator.java
@@ -9,4 +9,19 @@ public class Calculator {
         this.op1 = op1;
         this.op2 = op2;
     }
+
+    public Float Op(){
+        switch (sign){
+            case "+":
+                return op1 + op2;
+            case "-":
+                return op1 - op2;
+            case "/":
+                return op1 / op2;
+            case "*":
+                return op1 * op2;
+            default:
+                throw new IllegalArgumentException("the sign is not code yet");
+        }
+    }
 }
