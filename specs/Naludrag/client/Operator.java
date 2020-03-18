65b4fde7c8fbaa480bd8b098311708aa009f3210
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-03-18 16:04:12 +0100

    Operator class created

---
+++ specs/Naludrag/client/Operator.java
@@ -0,0 +1,21 @@
+package client;
+
+public enum Operator {
+    ADD, SUB, MULTIPLY, DIVIDE;
+
+    @Override
+    public String toString() {
+        switch (Operator.this) {
+            case ADD:
+                return "+";
+            case SUB:
+                return "-";
+            case MULTIPLY:
+                return "*";
+            case DIVIDE:
+                return "/";
+            default:
+                return "";
+        }
+    }
+}
