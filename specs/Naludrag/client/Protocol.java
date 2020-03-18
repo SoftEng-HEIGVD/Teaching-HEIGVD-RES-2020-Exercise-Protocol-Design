b565cbb28633a5087e9a33c1f8fabb5a138ea649
Robin Müller <33422394+AziXus@users.noreply.github.com>
2020-03-18 15:55:06 +0100

    Protocol added

---
+++ specs/Naludrag/client/Protocol.java
@@ -0,0 +1,15 @@
+package client;
+
+/**
+ *
+ * @author Olivier Liechti
+ * @author Robin Müller
+ */
+public class Protocol {
+
+    public static final int PRESENCE_DEFAULT_PORT = 12302;
+
+    public static final String CMD_HELLO = "HELLO";
+    public static final String CMD_BYE = "BYE";
+
+}
