db1c1d26709cbb72dc69485a42e5e8489eff2526
jerome arn <jerome.arn@heig-vd.ch>
2020-03-18 22:03:24 +0100

    serveur et calculette done

--- specs/hansArn/src/Calculator.java
+++ specs/hansArn/src/Calculator.java
@@ -4,12 +4,22 @@ public class Calculator {
     private Float op1;
     private Float op2;
 
+    /**
+     * configure for a calcul with two operands
+     * @param sign operation to do
+     * @param op1 first operand
+     * @param op2 second operand
+     */
     public Calculator(String sign, Float op1, Float op2) {
         this.sign = sign;
         this.op1 = op1;
         this.op2 = op2;
     }
 
+    /**
+     * do the operation
+     * @return result of the operation
+     */
     public Float Op(){
         switch (sign){
             case "+":
--- specs/hansArn/src/Server.java
+++ specs/hansArn/src/Server.java
@@ -5,11 +5,24 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Server {
     int port;
+    int timeOut;
+
+    /**
+     * Constructor
+     * @param port the port to listen on
+     * @param timeOutInSecond set timeout in seconds
+     */
+    public Server(int port, int timeOutInSecond) {
+        this.port = port;
+        this.timeOut = timeOutInSecond * 1000;
+    }
 
     /**
      * Constructor
@@ -17,7 +30,9 @@ public class Server {
      */
     public Server(int port) {
         this.port = port;
+        this.timeOut = 0;
     }
+
 
     public void serveClients() {
         ServerSocket serverSocket;
@@ -35,36 +50,59 @@ public class Server {
 
         while (true) {
             try {
+                // configuration du client
                 clientSocket = serverSocket.accept();
+                clientSocket.setSoTimeout(timeOut);
+                // configuration des IOs
                 in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 out = new PrintWriter(clientSocket.getOutputStream());
                 String line;
+                // booléen d'arrêt
                 boolean shouldRun = true;
 
+                // message d'acceuil
                 out.println("Welcome to calculator Server.\nSend me something to calculate with the form \" number sign number \" and conclude with the BYE command.\n" +
                         "4 operations are available (+, -, \\, *)");
                 out.flush();
+
                 while ( (shouldRun) && (line = in.readLine()) != null ) {
+                    // si le client dit bye on ferme la connexion
                     if (line.equalsIgnoreCase("bye")) {
                         shouldRun = false;
-                    }
-                    out.print("> ");
-                    String[] argumentArray = line.split(" ");
-                    if (argumentArray.length != 3){
-                        out.println("ERROR : Bad Use");
-                        out.flush();
-                        clientSocket.close();
-                        in.close();
-                        out.close();
                     }else{
-                        try {
-                            Calculator C = new Calculator(argumentArray[1], Float.parseFloat(argumentArray[0]), Float.parseFloat(argumentArray[2]));
-                        }catch (NumberFormatException e){
-                            out.println("ERROR : Bad Argument");
+                        out.print("> ");
+                        String[] argumentArray = line.split(" ");
+                        // si moins de 3 arguments on ferme la connexion
+                        if (argumentArray.length != 3){
+                            out.println("ERROR : Bad Use");
                             out.flush();
                             clientSocket.close();
                             in.close();
                             out.close();
+                        }else {
+                            try {
+                                Calculator C = new Calculator(argumentArray[1], Float.parseFloat(argumentArray[0]), Float.parseFloat(argumentArray[2]));
+                                out.println(C.Op());
+                                out.flush();
+                            }
+                            // dans le cas ou la transformation en float échoue
+                            catch (NumberFormatException e) {
+                                out.println("ERROR : Bad Argument");
+                                out.flush();
+                                clientSocket.close();
+                                in.close();
+                                out.close();
+
+                            }
+                            // dans le cas d'un signe non implémenté
+                            catch (IllegalArgumentException e) {
+                                out.println("ERROR : Bad Argument");
+                                out.flush();
+
+                                clientSocket.close();
+                                in.close();
+                                out.close();
+                            }
                         }
                     }
                 }
@@ -73,7 +111,17 @@ public class Server {
                 in.close();
                 out.close();
 
-            } catch (IOException ex) {
+            } catch (SocketTimeoutException e ){
+                out.println("Timeout");
+                out.flush();
+                try {
+                    clientSocket.close();
+                    in.close();
+                    out.close();
+                }catch (IOException error){
+                    System.out.println(error);
+                }
+            }catch (IOException ex) {
                 if (in != null) {
                     try {
                         in.close();
