9cbb35be180407588a89fee635e9f781fe762066
Brunisholz Julien <julien.brunisholz@gmail.com>
2020-03-20 00:40:58 +0100

    client fonctionnel

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,5 +1,49 @@
 package client;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Client
 {
+    private final int PORT = 50000;
+    private BufferedReader is;
+    private BufferedWriter os;
+    private Socket socket;
+
+    public Client(String host) throws IOException {
+        socket = new Socket(host, PORT);
+        is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        os = new BufferedWriter(new PrintWriter(socket.getOutputStream()));
+        sendRequest("OPEN");
+        if(!is.readLine().equals("ACK")){
+            terminate();
+        }
+    }
+
+
+    public void terminate() throws IOException {
+        socket.close();
+        is.close();
+        os.close();
+    }
+
+    public void sendRequest(String request) throws IOException {
+        os.write(request);
+        os.flush();
+    }
+
+    public void verifyResponse() throws IOException {
+        String r = is.readLine();
+        if(r.equals("CLOSE")){
+            terminate();
+        }
+        if(r.equals("INVALID FORMAT")){
+            terminate();
+            throw new IOException("Invalid request format");
+        }
+        if(r.matches("^[0-9]*$")){
+            System.out.println(r);
+        }
+        throw new IOException("Error while receiving from server");
+    }
 }
