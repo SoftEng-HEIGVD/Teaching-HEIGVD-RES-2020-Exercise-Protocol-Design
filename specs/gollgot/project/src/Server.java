e121af271695296cea54b80b20685761406e85f5
Loic Dessaules <loic.dessaules@heig-vd.ch>
2020-03-19 18:34:08 +0100

    Implements multithreaded server to do simple calculation

--- specs/gollgot/project/src/Server.java
+++ specs/gollgot/project/src/Server.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Server {
 
@@ -58,16 +59,22 @@ public class Server {
 
     }
 
+    /**
+     * This inner class implements the behavior of the "servants", whose
+	 * responsibility is to take care of clients once they have connected. This
+	 * is where we implement the application protocol logic, more details about the protocol
+     * into the protocol.md file
+     */
     private class ServantWorker implements Runnable {
 
         Socket clientSocket;
         BufferedReader in = null;
         PrintWriter out = null;
 
-        public ServantWorker(Socket clientSocket) {
+        ServantWorker(Socket clientSocket) {
             this.clientSocket = clientSocket;
             try {
-                this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
                 this.out = new PrintWriter(clientSocket.getOutputStream());
             } catch (IOException e) {
                 e.printStackTrace();
@@ -75,8 +82,61 @@ public class Server {
             }
         }
 
-        private int calculate(String line){
-            return 42;
+        /**
+         * Calculate a basic math calculation that respect this format : <nb1> <space> <operator> <space> <nb2>
+         * And the operator can be +, -, *, /
+         * @param calculation The String line contains the calculation
+         * @return "invalid" if an error occurred, the result as a String otherwise
+         */
+        private String calculate(String calculation){
+            int nb1 = 0;
+            int nb2 = 0;
+            int answer = 0;
+            String operation;
+            String[] parts = calculation.split(" ");
+            boolean error = false;
+
+            // Calculate only NB Operation NB => so 3 parts
+            if(parts.length != 3) {
+                error = true;
+            } else{
+                try {
+                    nb1 = Integer.parseInt(parts[0]);
+                    nb2 = Integer.parseInt(parts[2]);
+                } catch(NumberFormatException e){
+                    System.out.println("Error occurred on calculation parsing : " + e.getMessage());
+                    error = true;
+                }
+
+                if(!error) {
+                    operation = parts[1];
+                    switch (operation) {
+                        case "+":
+                            answer = nb1 + nb2;
+                            break;
+                        case "-":
+                            answer = nb1 - nb2;
+                            break;
+                        case "*":
+                            answer = nb1 * nb2;
+                            break;
+                        case "/":
+                            try{
+                                answer = nb1 / nb2;
+                            }catch(ArithmeticException e){
+                                System.out.println("Error occurred, arithmetic exception : " + e.getMessage());
+                                error = true;
+                            }
+                            break;
+                        default:
+                            error = true;
+                            break;
+                    }
+                }
+
+            }
+
+            return error ? "invalid" : String.valueOf(answer);
         }
 
 
@@ -90,22 +150,26 @@ public class Server {
             out.flush();
 
             try {
-
                 System.out.println("Reading until client sends BYE or close connection ...");
                 while((shouldRun && (line = in.readLine()) != null)){
-
                     // Client ask for calculation
-                    if(line.equalsIgnoreCase("calculation")){
-                        out.println("Ready for calculation");
+                    if(line.equalsIgnoreCase("hello")){
+                        out.println("Ready");
                         out.flush();
                         // Loop through all calculation the client want to do
                         while(shouldRun && (line = in.readLine()) != null){
                             // Client ask to quit
                             if(line.equalsIgnoreCase("bye")){
                                 shouldRun = false;
-                            }else{
-                                int result = calculate(line);
-                                out.println("> " + result);
+                            }
+                            // Otherwise client may ask for a calculation
+                            else{
+                                String result = calculate(line);
+                                if(!result.equals("invalid")){
+                                    out.println(result);
+                                }else{
+                                    out.println("Error");
+                                }
                                 out.flush();
                             }
                         }
@@ -113,9 +177,12 @@ public class Server {
 
                     // Client ask to quit
                     if(line.equalsIgnoreCase("bye")){
-                        shouldRun = false;
+                        out.println("Bye");
+                        out.flush();
+                        break;
                     }
-                    out.println("> " + line.toUpperCase());
+
+                    out.println("Error");
                     out.flush();
                 }
 
@@ -125,6 +192,7 @@ public class Server {
                 out.close();
 
             } catch (IOException e) {
+                // Correctly close the BufferedReader
                 if(in != null){
                     try {
                         in.close();
@@ -132,9 +200,13 @@ public class Server {
                         ex.printStackTrace();
                     }
                 }
+
+                // Correctly close the PrintWriter
                 if(out != null){
                     out.close();
                 }
+
+                // Correctly close the Socket
                 if(clientSocket != null) {
                     try {
                         clientSocket.close();
