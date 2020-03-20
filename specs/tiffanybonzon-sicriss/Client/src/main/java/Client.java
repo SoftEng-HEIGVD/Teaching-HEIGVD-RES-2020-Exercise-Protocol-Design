6cb92d6ce30dbd77b62a4758ac13d475cec8ea1f
Sicriss <laurent.thoeny@gmail.com>
2020-03-20 14:59:29 +0100

    Refractor Client + Tests template

--- specs/tiffanybonzon-sicriss/Client/src/main/java/Client.java
+++ specs/tiffanybonzon-sicriss/Client/src/main/java/Client.java
@@ -4,63 +4,63 @@ import java.util.Scanner;
 
 public class Client
 {
-    Socket socket;
-    BufferedReader in = null;
-    PrintWriter out = null;
-    Scanner scan = null;
+    static Socket socket;
+    static BufferedReader in = null;
+    static PrintWriter out = null;
+    static Scanner scan = null;
 
-    public void main(String[] args)
+    private static void init() throws IOException
+    {
+        System.out.println("Welcome to the calculator !\n");
+
+        System.out.println("Trying to establish a socket !");
+        socket = new Socket("127.0.0.1", 2112);
+
+        System.out.println("Socket established, setting up the IO stuff");
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream());
+
+        System.out.println("IO stuff seems to work, sending greetings to the server.\n");
+        out.println("HELLO"); out.flush();
+    }
+
+    private static void worker() throws Exception
+    {
+        String line;
+        scan = new Scanner(System.in);
+        boolean end = false;
+
+        while (!end)
+        {
+            while ((line = in.readLine()) != null)
+            {
+                if (line.equals("> ERROR"))
+                {
+                    throw new Exception("The server responded with an error");
+                }
+
+                System.out.println(line);
+
+                System.out.println("Please enter a command");
+                String command = scan.nextLine();
+
+                if (command.startsWith("END"))
+                {
+                    end = true;
+                    System.out.println("Very well, exiting now. Goodbye");
+                }
+
+                out.println(command); out.flush();
+            }
+        }
+    }
+
+    public static void main(String[] args)
     {
         try
         {
-            System.out.println("Welcome to the calculator !");
-
-            System.out.println("\nTrying to establish a socket !");
-            this.socket = new Socket("127.0.0.1", 2112);
-
-            System.out.println("Socket established, setting up the IO stuff");
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out = new PrintWriter(socket.getOutputStream());
-
-            System.out.println("IO stuff seems to work, sending greetings to the server.");
-            out.println("Hello"); out.flush();
-
-            String line;
-            scan = new Scanner(System.in);
-            boolean end = false;
-
-            while (!end)
-            {
-                while ((line = in.readLine()) != null)
-                {
-                    if (line.equals("Error"))
-                    {
-                        throw new Exception("The server responded with an error");
-                    }
-
-                    if (line.startsWith("OPs : "))
-                    {
-                        System.out.println("Connexion established with the server.");
-                        System.out.println("The available commands are : " + line.substring(6) + " & END");
-                    }
-                    else
-                    {
-                        System.out.println("The result is " + line);
-                    }
-
-                    System.out.println("Please enter a command");
-                    String command = scan.nextLine();
-
-                    if (command.startsWith("END"))
-                    {
-                        end = true;
-                        System.out.println("Very well, exiting now. Goodbye");
-                    }
-
-                    out.println(command); out.flush();
-                }
-            }
-
+            init();
+            worker();
         }
         catch (Exception e)
         {
--- specs/tiffanybonzon-sicriss/Client/src/test/java/ClientTest.java
+++ specs/tiffanybonzon-sicriss/Client/src/test/java/ClientTest.java
@@ -1,2 +1,12 @@
-public class ClientTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ClientTest
+{
+    @Test
+    void testTemplateForLater()
+    {
+        assertEquals("1", "1");
+    }
 }
