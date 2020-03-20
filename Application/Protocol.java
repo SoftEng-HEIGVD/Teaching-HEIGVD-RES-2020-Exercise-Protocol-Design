78296ca3608ededef6cf0e6eacde155193cc0777
Rosy-Laure Wonjamouna <laure.wonjamouna@gmail.com>
2020-03-20 12:27:50 +0100

    Protocol.java is set

---
+++ Application/Protocol.java
@@ -0,0 +1,16 @@
+package ch.heigvd.res.examples;
+
+/**
+ * Protocol.java file based on the protocol given by Olivier Liechti as an example in class
+ * @author Mathias Maillard
+ * @author Rosy-Laure Wonjamouna
+ */
+public class Protocol {
+
+    public static final int PRESENCE_DEFAULT_PORT = 1000;
+
+    public static final String CMD_SYN = "SYN";
+    public static final String CMD_SYN_ACK = "SYN-ACK";
+    public static final String CMD_ACK = "ACK";
+    public static final String CMD_FIN = "FIN";
+}
