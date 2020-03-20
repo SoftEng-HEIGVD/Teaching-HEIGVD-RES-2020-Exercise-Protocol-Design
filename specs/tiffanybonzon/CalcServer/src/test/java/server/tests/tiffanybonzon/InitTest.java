b9d5422da289bc9e6e4b936ee9c82f0d89b20789
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-03-20 15:15:57 +0100

    Server implemented but not yet unit tested

---
+++ specs/tiffanybonzon/CalcServer/src/test/java/server/tests/tiffanybonzon/InitTest.java
@@ -0,0 +1,12 @@
+package server.tests.tiffanybonzon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class InitTest {
+    @Test
+    void theServerShouldSendOPListWhenReceivingHello() {
+        //Not sure how to test...will look into it later
+    }
+}
