dcb0f80342aae4c745d8e47d804fce7dbcecfec0
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-03-21 17:36:32 +0100

    Remove infinite loop in Client's main

--- specs/alexandrepiveteau-glsubri/code/src/main/java/client/Main.java
+++ specs/alexandrepiveteau-glsubri/code/src/main/java/client/Main.java
@@ -8,8 +8,6 @@ public class Main {
   public static void main(String[] args) throws IOException {
     Client client = new Client(InetAddress.getLocalHost());
 
-    while (true) {
-      client.start();
-    }
+    client.start();
   }
 }
