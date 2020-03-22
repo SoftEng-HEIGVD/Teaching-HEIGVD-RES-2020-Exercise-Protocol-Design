a54c9eb01e321026e88e0a33a846034185a19a85
alfavre <fav.alban@gmail.com>
2020-03-22 22:55:36 +0100

    comment updated

--- alfavre-alfavre/client/src/main/java/ch/heigvd/res/App.java
+++ alfavre-alfavre/client/src/main/java/ch/heigvd/res/App.java
@@ -7,8 +7,6 @@ import java.net.InetAddress;
 public class App {
 
     /**
-     * Doesn't work with double, int only (idk why, but i wont fix)
-     * Does return the double division (3/6=0.5 and not 0)
      *
      * @param args the command line arguments
      */
--- alfavre-alfavre/server/src/main/java/ch/heigvd/res/App.java
+++ alfavre-alfavre/server/src/main/java/ch/heigvd/res/App.java
@@ -5,9 +5,6 @@ import java.io.IOException;
 public class App {
 
     /**
-     * server, I half assed the log, it logs almost nothing
-     * Doesn't work with double, int only (idk why, but i wont fix)
-     * Does return the double division (3/6=0.5 and not 0)
      *
      * @param args the command line arguments
      */
