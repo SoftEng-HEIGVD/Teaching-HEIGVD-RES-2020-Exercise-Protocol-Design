b5c6e04d49c9aa76d3ca59b92b7684dfef41e23b
G-Houlmann <gildas.houlmann@gmail.com>
2020-03-22 22:39:34 +0100

    final

--- src/RunServer.java
+++
@@ -1,8 +0,0 @@
-import calculatorServer.CalculatorServer;
-
-public class RunServer {
-    public static void main(String args[]){
-        CalculatorServer srv = new CalculatorServer(1777);
-        srv.serveClients();
-    }
-}
